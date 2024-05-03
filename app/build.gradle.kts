0378966274c17829ae869582d404b061365e084e
waseela123 <abbas.waseela2006@gmail.com>
2024-05-03 15:00:26 +0300

    بطل يشتغل عالتفلون افحصي

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.firebase:firebase-firestore:24.11.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
-    //implementation(project(":app"))
+   //implementation(project(":app"))
     testImplementation("junit:junit:4.13.2")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.squareup.picasso:picasso:2.71828")
